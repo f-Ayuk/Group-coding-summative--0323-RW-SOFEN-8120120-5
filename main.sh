09c55dfcb3606aac95b5353ccc3eadc47378022a
aadedigbae <a.adedigba@alustudent.com>
2023-06-24 10:07:35 -0700

    View and delete student record

--- main.sh
+++ main.sh
@@ -18,12 +18,41 @@ create_student() {
     echo "Student record created successfully."
 }
 
+# Function to view all inputed student records
+view_student_record() {
+	if [[ ! -f "$students_file" ]]; then
+		echo "No student record found"
+		return
+	fi 
+
+	echo "Records of students available"
+	cat "$students_file"
+}
+
+# Function to delete a student by ID
+delete_student_record_by_ID() {
+	echo "Enter student ID to be deleted:"
+	read student_id
+
+	# Create a temporary file for storing updated records
+    temp_file="temp.txt"
+    
+    # Remove the student record from the file
+    grep -v "^.*|$student_id$" "$students_file" > "$temp_file"
+    
+    # Replace the original file with the temporary file
+    mv "$temp_file" "$students_file"
+    
+    echo "Student with ID $student_id deleted successfully."
+}
 # Main menu loop
 while true; do
     echo "
     Bachelor of Software Engineering Cohort List
 
     1. Create student record
+    2. View Student Records
+    3. Delete Student Record
 
 Enter your choice:"
     
@@ -31,7 +60,13 @@ Enter your choice:"
     
     case $choice in
         1)
-            create_student
-            ;;
+		create_student
+            	;;
+	2)
+		view_student_record
+		;;
+	3)
+		delete_student_record_by_ID
+		;;
 esac
 done
